d2f9c27528b274989668af2148fb335dcbdf40d1
carrascomj <carrascomj@gmail.com>
2020-12-17 01:40:55 +0100

    docs: write down some comments

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ mod schemas;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    for id in ["199420", "114984"].iter() {
+    // 1. met ids from bigg
+    // TODO: parse bigg universal
+    let bigg = ["199420", "114984"];
+    for id in bigg.iter() {
+        // 2. request + parse
         let res = request::get_pathway(schemas::Met {
             id,
             species: "9606",
@@ -11,6 +15,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
         let path: Result<Vec<schemas::Pathway>,  serde_json::Error> = serde_json::from_str(res.as_str());
         match path {
+            // TODO: async write to file WIP
             Ok(p) => println!("Met id: {}\tPath id: {}", id, p[0].db_id),
             _ => println!("Requested id {} not found 404", id)
         }
